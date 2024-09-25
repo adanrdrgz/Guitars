58aceda574f0863349bc1199d3d65ae48b1204ac
Adan Rodriguez <aj.rdrgz444@gmail.com>
2024-09-24 22:13:38 -0300

    relatives paths

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -2,7 +2,7 @@ const Guitar = ({guitar, addToCart}) => {
     return <>
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
                 <div className="col-4">
-                    <img className="img-fluid" src={`./public/img/${guitar.image}.jpg`} alt="imagen guitarra" />
+                    <img className="img-fluid" src={`./img/${guitar.image}.jpg`} alt="imagen guitarra" />
                 </div>
                 <div className="col-8">
                     <h3 className="text-black fs-4 fw-bold text-uppercase">{guitar.name}</h3>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,14 @@ const Header = ({cart, removeFromCart, increaseQuantity, decreaseQuantity, clear
             <div className="row justify-content-center justify-content-md-between">
                 <div className="col-8 col-md-3">
                     <a href="index.html">
-                        <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
+                        <img className="img-fluid" src="./img/logo.svg" alt="imagen logo" />
                     </a>
                 </div>
                 <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                     <div 
                         className="carrito"
                     >
-                        <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+                        <img className="img-fluid" src="./img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" className="bg-white p-3">
                           {isEmpty && <p className="text-center">El carrito esta vacio</p>}  
@@ -33,7 +33,7 @@ const Header = ({cart, removeFromCart, increaseQuantity, decreaseQuantity, clear
                                     {cart.map(guitar => (
                                         <tr key={guitar.id}>
                                         <td>
-                                            <img className="img-fluid" src={`./public/img/${guitar.image}.jpg`} alt="imagen guitarra" />
+                                            <img className="img-fluid" src={`./img/${guitar.image}.jpg`} alt="imagen guitarra" />
                                         </td>
                                         <td>{guitar.name}</td>
                                         <td className="fw-bold">
