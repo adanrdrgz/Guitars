25ff4fafe79eebf6c3d86e1cddb62c0f457a342e
Adan Rodriguez <aj.rdrgz444@gmail.com>
2024-09-24 22:14:47 -0300

    unused functions

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   const MAX_ITEMS = 5
   const MIN_ITEMS = 1
-  const [data, setData] = useState(db)
+  const [data] = useState(db)
   const [cart, setCart] = useState(initialCart)
   
   const addToCart = (item) => {
